f057da8a87c8c86e43403a3128010225c18025e4
vupa2 <anhvu.pa96@gmail.com>
2021-04-29 09:08:17 +0700

    Update table.sql

--- table.sql
+++ table.sql
@@ -5,11 +5,17 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL findAllCustomer();
-
 DELIMITER //
-DROP PROCEDURE IF EXISTS findAllCustomer;
+DROP PROCEDURE IF EXISTS findAllCustomer //
+CREATE PROCEDURE findAllCustomer()
 BEGIN
     SELECT * FROM customers WHERE customerNumber = 175;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE OR REPLACE PROCEDURE findALlCustomer()
+BEGIN
+    SELECT * FROM customers WHERE customerNumber = 166;
 END //
 DELIMITER ;
